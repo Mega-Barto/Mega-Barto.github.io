e37f8a2d3642bf283718381c7b547bd2e2172288
Mega-Barto <a.perez3@utp.edu.co>
2025-07-14 20:31:44 -0500

    feat: Update certificates with new entries and internationalization support

--- src/contents/certificates.ts
+++ src/contents/certificates.ts
@@ -1,32 +1,39 @@
 import type { TFunction } from 'i18next';
 
 export interface Certificate {
-  id: string;
-  title: string;
-  academy: string;
-  dateObtained: string;
-  verificationCode?: string;
+    id: string;
+    title: string;
+    academy: string;
+    dateObtained: string;
+    verificationCode?: string;
 }
 
 export const getCertificates = (t: TFunction): Certificate[] => [
-  {
-    id: '1',
-    title: t('certificates.react.title'),
-    academy: t('certificates.react.academy'),
-    dateObtained: t('certificates.react.dateObtained'),
-    verificationCode: 'REACT-2024-001'
-  },
-  {
-    id: '2',
-    title: t('certificates.typescript.title'),
-    academy: t('certificates.typescript.academy'),
-    dateObtained: t('certificates.typescript.dateObtained'),
-    verificationCode: 'TS-2023-456'
-  },
-  {
-    id: '3',
-    title: t('certificates.javascript.title'),
-    academy: t('certificates.javascript.academy'),
-    dateObtained: t('certificates.javascript.dateObtained')
-  }
+    {
+        id: '1',
+        title: t('certificates.automation.title'),
+        academy: t('certificates.automation.academy'),
+        dateObtained: t('certificates.automation.dateObtained')
+    },
+    {
+        id: '2',
+        title: t('certificates.django.title'),
+        academy: t('certificates.django.academy'),
+        dateObtained: t('certificates.django.dateObtained'),
+        verificationCode: '948d36fc-3952-434f-a701-b023f649f832'
+    },
+    {
+        id: '3',
+        title: t('certificates.testing.title'),
+        academy: t('certificates.testing.academy'),
+        dateObtained: t('certificates.testing.dateObtained'),
+        verificationCode: 'bcd89e6a-399d-4261-9a98-d52b9efc16ad'
+    },
+    {
+        id: '4',
+        title: t('certificates.docker.title'),
+        academy: t('certificates.docker.academy'),
+        dateObtained: t('certificates.docker.dateObtained'),
+        verificationCode: 'e759659f-40dc-4560-a84e-881b7cacf6d5'
+    }
 ];
